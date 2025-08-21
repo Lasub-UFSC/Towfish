7d6515cbbdc7b0ec880d8de5fa5e43fc66dc5ccb
unknown <luisedu.fernandes2002@gmail.com>
2025-08-21 16:18:16 -0300

    adding text box

--- Desktop/app/analise.py
+++ Desktop/app/analise.py
@@ -6,55 +6,53 @@ import matplotlib.pyplot as plt
 def convertData(data):
         convertedData= {}
 
-        byte_data = struct.pack('<HH', data[0], data[1])
+        byte_data = struct.pack('<HH', int(data[0]), int(data[1]))
         convertedData["timestamp"] = struct.unpack('<I', byte_data)[0]
 
-        byte_data = struct.pack('<HH', data[2], data[3])
+        byte_data = struct.pack('<HH', int(data[2]), int(data[3]))
         convertedData["accx"] = struct.unpack('<f', byte_data)[0]
 
-        byte_data = struct.pack('<HH', data[4], data[5])
+        byte_data = struct.pack('<HH', int(data[4]), int(data[5]))
         convertedData["accy"] = struct.unpack('<f', byte_data)[0]
 
-        byte_data = struct.pack('<HH', data[6], data[7])
+        byte_data = struct.pack('<HH', int(data[6]), int(data[7]))
         convertedData["accz"] = struct.unpack('<f', byte_data)[0]
 
-        byte_data = struct.pack('<HH', data[8], data[9])
+        byte_data = struct.pack('<HH', int(data[8]), int(data[9]))
         convertedData["gyrox"] = struct.unpack('<f', byte_data)[0]
 
 
-        byte_data = struct.pack('<HH', data[10], data[11])
+        byte_data = struct.pack('<HH', int(data[10]), int(data[11]))
         convertedData["gyroy"] = struct.unpack('<f', byte_data)[0]
 
 
-        byte_data = struct.pack('<HH', data[12], data[13])
+        byte_data = struct.pack('<HH', int(data[12]), int(data[13]))
         convertedData["gyroz"] = struct.unpack('<f', byte_data)[0]
 
         return convertedData
 
 
-df = pd.read_csv(r'data\2025-08-14--15.22.csv',header=None)
+df = pd.read_csv(r'data\2025-08-21--15.13.csv',header=None)
 timestamps = []
 for index, row in df.iterrows():
     result_array = convertData(row.values)
 
     timestamps.append(result_array["timestamp"])
 
-frequency=[]
+difference=[]
 
 for i in range(1, len(timestamps)):
     # Subtract the previous item from the current item
-    difference = timestamps[i] - timestamps[i - 1]
-    # Append the result to the new array
-    frequency.append(1000/difference)
+    difference.append(timestamps[i] - timestamps[i - 1])
+
 
 # Plot the results
 plt.figure(figsize=(10, 6))
-plt.plot(frequency, marker='o', linestyle='-')
-plt.title('Frequency of Data Requests 5 Hz')
-plt.xlabel('Request Index')
-plt.ylabel('Frequency (Hz)')
+plt.plot(difference, marker='o', linestyle='-')
 plt.grid(True)
 plt.show()
-
-# Save the plot
-plt.savefig('frequency_plot_5.png')
+plt.clf()
+plt.figure(figsize=(10, 6))
+plt.plot(timestamps, marker='o', linestyle='-')
+plt.grid(True)
+plt.show()
